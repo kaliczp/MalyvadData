2cad85702ccd3bff58f1c576b791018dd91620bd
Péter Kalicz <kaliczp@gmail.com>
2026-09-11 14:25:04 +0200

    Labels corrected
    Éveshez feltettem a mértékegységet

--- PrecipPlot.R
+++ PrecipPlot.R
@@ -5,15 +5,15 @@ par(xaxs = "i", yaxs = "i", las = 1, lend = 1)
 for(PrecipObject in PrecipObjects) {
     actual.xts <- get(PrecipObject)
     plot.zoo(actual.xts, type = "h", main = sub(".xts", "", PrecipObject),
-             xlab = "", ylab = "Csap. [mm]",
+             xlab = "", ylab = "Csapadék [mm/nap]",
              ylim = c(0,100)
              )
-    par(fig = c(0.5, 0.95, 0.65, 0.87), new = TRUE, mar = c(3.1, 2.1,0,0))
+    par(fig = c(0.5, 0.95, 0.65, 0.87), new = TRUE, mar = c(3.1, 4.1,0,0))
     PrecipYearly <- apply.yearly(actual.xts, sum)
     Years <- as.numeric(format(index(PrecipYearly), "%Y"))
     barplot(zoo(coredata(PrecipYearly), Years) , col = "lightblue",
             ylim = c(0, 1000),
-            ylab = "", xlab = "")
+            ylab = "[mm/év]", xlab = "")
     par(fig = c(0, 1, 0, 1), mar = c(5.1, 4.1, 4.1, 2.1))
 }
 dev.off()

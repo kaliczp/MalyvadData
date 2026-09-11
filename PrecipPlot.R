7df9201cfc5ab885d5875a9c1b1fe6be06f2b7b5
Péter Kalicz <kaliczp@gmail.com>
2026-09-11 14:24:32 +0200

    Yearly precipitation added
    Feltettem az éves csapadékokat is

--- PrecipPlot.R
+++ PrecipPlot.R
@@ -1,10 +1,19 @@
 PrecipObjects <- ls(patt = "P*xts")
 
 pdf(width = 14)
-par(xaxs = "i")
+par(xaxs = "i", yaxs = "i", las = 1, lend = 1)
 for(PrecipObject in PrecipObjects) {
-    plot.zoo(get(PrecipObject), type = "h", main = PrecipObject,
-             xlab = "", ylab = "Csap. [mm]"
+    actual.xts <- get(PrecipObject)
+    plot.zoo(actual.xts, type = "h", main = sub(".xts", "", PrecipObject),
+             xlab = "", ylab = "Csap. [mm]",
+             ylim = c(0,100)
              )
+    par(fig = c(0.5, 0.95, 0.65, 0.87), new = TRUE, mar = c(3.1, 2.1,0,0))
+    PrecipYearly <- apply.yearly(actual.xts, sum)
+    Years <- as.numeric(format(index(PrecipYearly), "%Y"))
+    barplot(zoo(coredata(PrecipYearly), Years) , col = "lightblue",
+            ylim = c(0, 1000),
+            ylab = "", xlab = "")
+    par(fig = c(0, 1, 0, 1), mar = c(5.1, 4.1, 4.1, 2.1))
 }
 dev.off()

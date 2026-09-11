9486d8c353b47bfdff31cc52df2f868a3202e634
Péter Kalicz <kaliczp@gmail.com>
2026-09-11 13:18:48 +0200

    Add more files and comments
    A maradék fájlok megadása

--- PrecipitationDownload.R
+++ PrecipitationDownload.R
@@ -1,15 +1,13 @@
 urls <- c(
     P67203 = "https://odp.met.hu/climate/observations_hungary/daily_rain/historical/HABP_1RD_67203_20020101_20251231_hist.zip",
-    P66523 = "https://odp.met.hu/climate/observations_hungary/daily_rain/historical/HABP_1RD_66523_20210101_20251231_hist.zip"
-)
-## 67207
-## 67113
+    P66523 = "https://odp.met.hu/climate/observations_hungary/daily_rain/historical/HABP_1RD_66523_20210101_20251231_hist.zip",
+    P67207 = "https://odp.met.hu/climate/observations_hungary/daily/historical/HABP_1D_67207_20120901_20251231_hist.zip",
+    P67113 = "https://odp.met.hu/climate/observations_hungary/daily/historical/HABP_1D_67113_20120901_20251231_hist.zip")
 ## 67522
 
 for (urlnr in 1:length(urls)) {
-
+  ## Zipfile név gyártás
   zipfilename <- paste0(names(urls)[urlnr], ".zip")
-
+  ## Letöltés
   download.file(urls[urlnr], zipfilename, mode = "wb")
-
 } 

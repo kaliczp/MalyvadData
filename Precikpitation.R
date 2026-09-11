b1dabde43042e0cffb407f6116339653ec385df7
Péter Kalicz <kaliczp@gmail.com>
2026-09-11 13:12:41 +0200

    Processing downloaded zip files
    Kiszedve a csapadék az adatokból és feldolgozva

--- Precikpitation.R
+++ Precikpitation.R
@@ -1,13 +1,15 @@
+library(xts)
 zipfiles <- dir(patt="zip")
 
 for (zipfile in zipfiles) {
-
-  csvfile <- unzip(zipfile, list = TRUE)$Name
-
-  unzip(zipfile, exdir = tempdir())
-
-  adat <- read.csv(file.path(tempdir(), csvfile))
-  
-  # itt dolgozhatsz az adat objektummal
-  print(head(adat))
+    ## Fájl név kinyerése
+    csvfile <- unzip(zipfile, list = TRUE)$Name
+    ## Kicsomagolás átmeneti könyvtárba
+    unzip(zipfile, exdir = tempdir())
+    ## Adatsor kibontása
+    adat <- read.table(file.path(tempdir(), csvfile), sep = ";", head = TRUE)
+    ## Végső objektumnév legyártása a zipfájl nevéből
+    finalobjectname <- paste0("P", adat[1, "StationNumber"], ".xts")
+    ## Idősor
+    assign(finalobjectname, xts(adat$r ,as.Date(as.character(adat$Time), format = "%Y%m%d")))
 }

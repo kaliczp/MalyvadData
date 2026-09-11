de85b23c667f76bc63a72c14ca6352146d81e37d
Péter Kalicz <kaliczp@gmail.com>
2026-09-11 16:00:52 +0200

    Recent year added
    Az idei évet is hozzáadtam

---
+++ CurrentDownload.R
@@ -0,0 +1,14 @@
+urls <- c(
+    P67203akt = "https://odp.met.hu/climate/observations_hungary/daily_rain/recent/HABP_1RD_67203_akt.zip",
+    P66523akt = "https://odp.met.hu/climate/observations_hungary/daily_rain/recent/HABP_1RD_66523_akt.zip",
+    P67207akt = "https://odp.met.hu/climate/observations_hungary/daily/recent/HABP_1D_67207_akt.zip",
+    P67113akt = "https://odp.met.hu/climate/observations_hungary/daily/recent/HABP_1D_67113_akt.zip",
+    P66522akt = "https://odp.met.hu/climate/observations_hungary/daily/recent/HABP_1D_66522_akt.zip"
+)
+
+for (urlnr in 1:length(urls)) {
+  ## Zipfile név gyártás
+  zipfilename <- paste0(names(urls)[urlnr], ".zip")
+  ## Letöltés
+  download.file(urls[urlnr], zipfilename, mode = "wb")
+} 
--- Precikpitation.R
+++ Precikpitation.R
@@ -1,6 +1,8 @@
 library(xts)
-zipfiles <- dir(patt="zip")
+allzipfiles <- dir(patt="zip")
+IsActual <- grep("akt", allzipfiles)
 
+zipfiles <- allzipfiles[-IsActual]
 for (zipfile in zipfiles) {
     ## Fájl név kinyerése
     csvfile <- unzip(zipfile, list = TRUE)$Name
@@ -13,6 +15,21 @@ for (zipfile in zipfiles) {
     ## Idősor
     assign(finalobjectname, xts(adat$r ,as.Date(as.character(adat$Time), format = "%Y%m%d")))
 }
+
+zipfiles <- allzipfiles[IsActual]
+for (zipfile in zipfiles) {
+    ## Fájl név kinyerése
+    csvfile <- unzip(zipfile, list = TRUE)$Name
+    ## Kicsomagolás átmeneti könyvtárba
+    unzip(zipfile, exdir = tempdir())
+    ## Adatsor kibontása
+    adat <- read.table(file.path(tempdir(), csvfile), sep = ";", head = TRUE)
+    ## Végső objektumnév legyártása a zipfájl nevéből
+    finalobjectname <- paste0("P", adat[1, "StationNumber"], ".xts")
+    ## Idősor
+    assign(finalobjectname, c(get(finalobjectname), xts(adat$r ,as.Date(as.character(adat$Time), format = "%Y%m%d"))))
+}
+
 
 ## Adathiány kezelése
 P67113.xts[P67113.xts < -990] <- NA

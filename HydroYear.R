314f8195d47fa35f8359ca431fc9131b6510fadb
Péter Kalicz <kaliczp@gmail.com>
2026-09-13 18:47:06 +0200

    Winter season on plot
    A téli félévet külön oszlopként felrakom az ábrára utólag

--- HydroYear.R
+++ HydroYear.R
@@ -31,10 +31,27 @@ annual_hydro <- HydYearUntilNow(AllPrec.xts, "08-31")
 ## Máig a folyamatosakkal
 annual_hydro <- HydYearUntilNow(AllPrec.xts["2014/",c("P66522.xts", "P67113.xts", "P67207.xts")])
 
+## Egy adatsor ábrázolása
 pdf(width = 14)
 for(actCol in colnames(annual_hydro[-1])) {
     Actual <- annual_hydro[,actCol]
     barplot(zoo(Actual, annual_hydro[,"HY"]), main = sub(".xts", "", actCol), col = "lightblue")
+    axis(2, as.numeric(tail(Actual,1)), tck = 1, lab = "")
+}
+dev.off()
+
+## Két adatsor egymáson ábrázolása
+pdf(width = 14)
+annual_hydro <- HydYearUntilNow(AllPrec.xts["2014/",c("P66522.xts", "P67113.xts", "P67207.xts")])
+winterseason_hydro <- HydYearUntilNow(AllPrec.xts["2014/",c("P66522.xts", "P67113.xts", "P67207.xts")], "04-30")
+for(actCol in colnames(annual_hydro[-1])) {
+    Actual <- annual_hydro[,actCol]
+    barplot(zoo(Actual, annual_hydro[,"HY"]),
+            main = sub(".xts", "", actCol), col = "orange")
+    barplot(zoo(winterseason_hydro[,actCol], winterseason_hydro[,"HY"]),
+            main = "", col = "lightblue",
+            xlab = "", ylab = "", axes = FALSE,
+            add = TRUE)
     axis(2, as.numeric(tail(Actual,1)), tck = 1, lab = "")
 }
 dev.off()
